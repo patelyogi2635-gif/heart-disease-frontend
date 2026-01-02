52e99a217ae08b4baf12c48a7be0b988fcf0c738
patelyogi2635-gif <patelyogi2635@gmail.com>
2026-01-02 16:46:13 +0530

    Add files via upload

---
+++ frontend/main.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import pandas as pd
+import joblib
+import requests
+API_URL = " https://heart-disease-backend-6-q9cc.onrender.com/predict"
+
+
+# Load saved model, scaler, and expected columns
+model = joblib.load("../backend/KNN_heart.pkl")
+scaler = joblib.load("../backend/scaler.pkl")
+expected_columns = joblib.load("../backend/columns.pkl")
+
+st.title("Heart Stroke Prediction by Yogi Patel")
+st.markdown("Provide the following details to check your heart stroke risk:")
+
+# Collect user input
+age = st.slider("Age", 18, 100, 40)
+sex = st.selectbox("Sex", ["M", "F"])
+chest_pain = st.selectbox("Chest Pain Type", ["ATA", "NAP", "TA", "ASY"])
+resting_bp = st.number_input("Resting Blood Pressure (mm Hg)", 80, 200, 120)
+cholesterol = st.number_input("Cholesterol (mg/dL)", 100, 600, 200)
+fasting_bs = st.selectbox("Fasting Blood Sugar > 120 mg/dL", [0, 1])
+resting_ecg = st.selectbox("Resting ECG", ["Normal", "ST", "LVH"])
+max_hr = st.slider("Max Heart Rate", 60, 220, 150)
+exercise_angina = st.selectbox("Exercise-Induced Angina", ["Y", "N"])
+oldpeak = st.slider("Oldpeak (ST Depression)", 0.0, 6.0, 1.0)
+st_slope = st.selectbox("ST Slope", ["Up", "Flat", "Down"])
+
+if st.button("Predict"):
+    payload = {
+        "age": age,
+        "sex": sex,
+        "chest_pain": chest_pain,
+        "resting_bp": resting_bp,
+        "cholesterol": cholesterol,
+        "fasting_bs": fasting_bs,
+        "resting_ecg": resting_ecg,
+        "max_hr": max_hr,
+        "exercise_angina": exercise_angina,
+        "oldpeak": oldpeak,
+        "st_slope": st_slope
+    }
+
+    response = requests.post(API_URL, json=payload)
+
+    if response.status_code == 200:
+        result = response.json()
+
+        if result["prediction"] == 1:
+            st.error("⚠️ Heart Disease Detected")
+        else:
+            st.success("✅ No Heart Disease Detected")
+    else:
+        st.warning("Backend API not responding")
